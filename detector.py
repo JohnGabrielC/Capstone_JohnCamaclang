5a47787475678d2f805ad25df99699e3de55b342
John Camaclang <JCamaclang15@students.desu.edu>
2022-02-03 13:48:58 -0500

    save image, removed landmarks, fix connections

--- detector.py
+++ detector.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-from mediapipe.framework.formats import landmark_mp2
 class Detector:
     def __init__(self, static_mode = False, model_complex = 1, smooth_landmark = True, segmentation = False,
     smooth_segment = False, min_detect_conf = 0.6, min_track_conf = 0.6):
@@ -14,6 +13,7 @@ class Detector:
 
         self.mp_draw = mp.solutions.drawing_utils
         self.mp_pose = mp.solutions.pose
+        self.mp_drawing_styles = mp.solutions.drawing_styles
 
         self.pose = self.mp_pose.Pose(self.static_mode, self.model_complex, self.smooth_landmark,
         self.segmentation, self.smooth_segment, 
@@ -24,13 +24,42 @@ class Detector:
 
 
     def get_pose(self, img, draw = True):
+        from mediapipe.framework.formats import landmark_pb2
 
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         self.res = self.pose.process(img)
+        self.landmarks = self.res.pose_landmarks
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        if self.res.pose_landmarks:
+
+        self.pose_connections = self.mp_pose.POSE_CONNECTIONS
+
+        #Only display the shoulders and part of the arms
+        self.landmark_subset = landmark_pb2.NormalizedLandmarkList(
+            landmark = [
+                self.landmarks.landmark[11],
+                self.landmarks.landmark[12],
+                self.landmarks.landmark[13],
+                self.landmarks.landmark[14]
+            ]
+        )
+        if self.landmarks:
             if draw:
-                self.mp_draw.draw_landmarks(img, self.res.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+                #Draws only the shoulders and part of the arms
+                #TODO: Figure out how to draw the lines back in without error                
+                self.mp_draw.draw_landmarks(img, 
+                    self.landmark_subset, 
+                    # self.pose_connections,
+                    landmark_drawing_spec= self.mp_drawing_styles.get_default_pose_landmarks_style())
+                # self.mp_draw.draw_landmarks(img, self.res.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+                
+                #Backup line
+                # self.mp_draw.draw_landmarks(img, 
+                #     self.landmarks, 
+                #     self.pose_connections,
+                #     landmark_drawing_spec= self.mp_drawing_styles.get_default_pose_landmarks_style())
+
+
         return img
 
     def detect_sitting(self, img):
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+from numpy import save
 from detector import Detector
 import cv2
 import mediapipe as mp
-from mediapipe.framework.formats import landmark_mp2
+import os
+
+def save_image(counter, img):
+    path = "C:/Users/John/Documents/School/Capstone"
+    cv2.imwrite(os.path.join(path, "frame_%d.jpg" % counter), img)
+    print("frame_%d" % counter + " saved")
 
 def main():
     cap = cv2.VideoCapture(0)
     detector = Detector()
-
+    count = 0
     while cap.isOpened():
         succ, img = cap.read()
         if not succ:
             print("Check camera, no output")
-            continue
-        
+            break
+
         cv2.imshow("Posture Detector", detector.get_pose(img))
         if cv2.waitKey(5) & 0xFF == 27:
             break
+        #Check if key is either spacebar or esc
+        a = cv2.waitKey(5)
+        if a % 256 == 32:
+            save_image(count, detector.get_pose(img))
+            count += 1
+
     cap.release()
-        
+    cv2.destroyAllWindows
 main()
