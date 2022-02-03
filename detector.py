4a86c72e652e3953d4bf66a2a28a6bc569cef016
John Camaclang <JCamaclang15@students.desu.edu>
2022-02-03 12:30:18 -0500

    Revert "First commit"
    This reverts commit b406580fddcff603791daddd51676cbcf4765862.

--- detector.py
+++ detector.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-
+from mediapipe.framework.formats import landmark_mp2
 class Detector:
     def __init__(self, static_mode = False, model_complex = 1, smooth_landmark = True, segmentation = False,
     smooth_segment = False, min_detect_conf = 0.6, min_track_conf = 0.6):
@@ -20,13 +20,38 @@ class Detector:
         self.min_detect_conf, self.min_track_conf
         )
 
+        
+
 
     def get_pose(self, img, draw = True):
+
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         self.res = self.pose.process(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         if self.res.pose_landmarks:
             if draw:
-                self.mp_draw.draw_landmarks(img, self.res.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
-
+                self.mp_draw.draw_landmarks(image = img, landmark_list = self.larndmark_subset)
         return img
+
+    def detect_sitting(self, img):
+        #TODO: Work on classification model/data
+        pass
+
+
+if __name__ == 'main':
+    def main():
+        cap = cv2.VideoCapture(0)
+        detector = Detector()
+
+        while cap.isOpened():
+            succ, img = cap.read()
+            if not succ:
+                print("Check camera, no output")
+                continue
+            
+            cv2.imshow("Posture Detector", detector.get_pose(img))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+        cap.release()
+        
+    main()
